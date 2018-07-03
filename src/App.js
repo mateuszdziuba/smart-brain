ef89cb01a1f534116a0cfbc40e76204276e554cf
mateuszdziuba <mkdziuba@gmail.com>
2018-07-03 16:24:03 +0200

    clear sessionStorage on sign out

--- src/App.js
+++ src/App.js
@@ -160,6 +160,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
+      window.sessionStorage.clear();
       return this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
