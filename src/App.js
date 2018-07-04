d46d8bb88a89c28bc19f1ab8c01b58eaac66c10d
mateuszdziuba <mkdziuba@gmail.com>
2018-07-04 14:42:55 +0200

    added aws lambda based ranking system

--- src/App.js
+++ src/App.js
@@ -150,7 +150,7 @@ class App extends Component {
           })  
             .then(response => response.json())
             .then(count => {
-              this.setState(Object.assign(this.state.user, {entries: count}))
+              this.setState(prevState=>(Object.assign(prevState.user, {entries: count})))
             })
             .catch(console.log);
         }
